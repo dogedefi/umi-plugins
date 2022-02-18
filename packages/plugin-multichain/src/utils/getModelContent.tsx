6b13ec4f89ba80081958097801805a91f2cb42c5
dogedefi <myz.defi@gmail.com>
2022-02-18 14:59:21 +0800

    refactor: initChainModel

--- packages/plugin-multichain/src/utils/getModelContent.tsx
+++ packages/plugin-multichain/src/utils/getModelContent.tsx
@@ -2,31 +2,9 @@
 export default () => `
 import { useState, useEffect } from 'react';
 import { Models } from '../../plugin-model/useModel';
-import { chainLocalKey, setupNetwork, Chain as DataType, chains } from 'web3-connector'
+import { initChainModel } from 'web3-connector'
 export type Chain = Models<'@@chain'>;
-const defaultChain = { name: 'BSC', config: chains.BSC }
 export default () => {
-  const [chain, setChain] = useState<DataType | undefined>()
-
-  // initial chain config
-  useEffect(() => {
-    let chainConfig = localStorage.getItem(chainLocalKey)
-
-    if (!chainConfig) {
-      setChain(defaultChain)
-      setupNetwork(defaultChain)
-    }
-    // must be a json object
-    else if (chainConfig.startsWith('{') && chainConfig.endsWith('}')) {
-      const cachedChain = JSON.parse(chainConfig)
-      setChain(cachedChain)
-      setupNetwork(cachedChain)
-    } else {
-      localStorage.clear()
-      location.reload()
-    }
-  }, [])
-
-  return { chain, setChain }
+  return initChainModel();
 }
 `
