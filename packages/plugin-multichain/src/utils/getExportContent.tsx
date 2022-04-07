40ad72c7f6e2062d344831e5659727cd57ef7b78
dogedefi <myz.defi@gmail.com>
2022-04-07 22:38:27 +0800

    plugin-multichain: __PLUGIN_MULTICHAIN

--- packages/plugin-multichain/src/utils/getExportContent.tsx
+++ packages/plugin-multichain/src/utils/getExportContent.tsx
@@ -3,5 +3,5 @@ export default (modelPath: string) => `
 // @ts-ignore
 import { Chain as ChainType } from '../${modelPath}';
 export type Chain = ChainType;
-export const __PLUGIN_INITIAL_STATE = 1;
+export const __PLUGIN_MULTICHAIN = 1;
 `;
